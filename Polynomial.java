56c101933ccc3a94cbba6e20862d81825bff7050
niiicky <nickylam234@gmail.com>
2024-05-13 03:21:39 -0400

    added Polynomial.java and Driver.java

---
+++ Polynomial.java
@@ -0,0 +1,47 @@
+public class Polynomial {
+	// fields
+	double coefficients[];
+	
+	
+	// constructor
+	public Polynomial() {
+		coefficients = new double[1];
+		coefficients[0] = 0;
+	}
+	
+	public Polynomial(double coefficients[]) {
+		this.coefficients = new double[coefficients.length];
+		
+		for(int i = 0; i < coefficients.length; i++) {
+			this.coefficients[i] = coefficients[i];
+		}
+	}
+	
+	
+	// methods
+	public Polynomial add(Polynomial a) {
+		double sumOfPolynomials[] = new double[Math.max(this.coefficients.length, a.coefficients.length)];
+		
+		for(int i = 0; i < this.coefficients.length; i++) {
+			sumOfPolynomials[i] += this.coefficients[i];
+		}
+		
+		for(int i = 0; i < a.coefficients.length; i++) {
+			sumOfPolynomials[i] += a.coefficients[i];
+		}
+		
+		return new Polynomial(sumOfPolynomials);
+	}
+	
+	public double evaluate(double x) {
+		double count = 0;
+		for(int i = 0; i < coefficients.length; i++) {
+			count += (coefficients[i] * Math.pow(x, i));
+		}
+		return count;
+	}
+	
+	public boolean hasRoot(double x) {
+		return (evaluate(x) == 0);
+	}
+}
